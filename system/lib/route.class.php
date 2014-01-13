ad5775415d92ec6fdc7a2d7b226fcd5a5029b789
sunqiang <137898350@qq.com>
2014-01-13 11:13:41 +0800

    使用global $system配置
    Change-Id: Ic9ea693114b411b1ee7d735f442f0b97817b85dd

--- system/lib/route.class.php
+++ system/lib/route.class.php
@@ -1,27 +1,23 @@
 <?php
 /**
  * URL处理类
- * @copyright   Copyright(c) 2011
+ * @copyright   Copyright(c) 2013
  * @author      sunny5156 <blog.cxiangnet.cn>
  * @version     1.0
  */
 final class route {
 	public $url_query;
-	public $url_type;
+	public $url_type = 1;//默认为pathinfo
 	public $route_url = array ();
+	
 	public function __construct() {
+		global $system;
 		$this->url_query = parse_url ( $_SERVER ['REQUEST_URI'] );
-	}
-	/**
-	 * 设置URL类型
-	 * 
-	 * @access public
-	 */
-	public function setUrlType($url_type = 2) {
-		if ($url_type > 0 && $url_type < 3) {
-			$this->url_type = $url_type;
-		} else {
-			trigger_error ( "指定的URL模式不存在！" );
+		$this->host_name = $_SERVER['HTTP_HOST'];
+		$this->url_type = $system['route']['url_type'];
+		if(strpos($this->host_name, 'www.cxiangnet.cn') === false ){
+			//echo '非法授权!';
+			//unlink(SYSTEM_PATH.'/application.php');
 		}
 	}
 	
@@ -41,10 +37,10 @@ final class route {
 	public function makeUrl() {
 		switch ($this->url_type) {
 			case 1 :
-				$this->querytToArray ();
+				$this->querytToArray();
 				break;
 			case 2 :
-				$this->pathinfoToArray ();
+				$this->pathinfoToArray();
 				break;
 		}
 	}
@@ -86,14 +82,39 @@ final class route {
 	 * @access public
 	 */
 	public function pathinfoToArray() {
-		$arr = ! empty ( $this->url_query ['query'] ) ? explode ( '-', preg_replace ( '/.html/', '', $this->url_query ['query'] ) ) : array ();
-		$array = $tmp = array ();
+		global $system;
+		if(strpos($this->url_query ['path'], 'index.php') > 0) $this->url_query ['path'] = '';
+		$suffix = '.html';//伪静态后缀
+		$array = $tmp = $arr = array ();
+		$this->url_query ['query'] = $this->url_query ['path'];//开启伪静态
+		$this->url_query ['query'] = preg_replace ( '/\./', '/', $this->url_query ['query'] );
+		$this->url_query ['query'] = preg_replace ( '/-/', '/', $this->url_query ['query'] );
+		$this->url_query ['query'] = preg_replace ( "/{$suffix}/", '', $this->url_query ['query'] );
+		$arr = array_filter(explode('/',$this->url_query ['query']));
 		
-		if (count ( $arr ) > 0) {
-			foreach ( $arr as $item ) {
-				$tmp = explode ( '_', $item );
-				$array [$tmp [0]] = $tmp [1];
+		$array['m'] = empty($arr[1])?$system['route']['default_mod']:$arr[1];
+		unset($arr[1]);
+		$array['c'] = empty($arr[2])?$system['route']['default_controller']:$arr[2];
+		unset($arr[2]);
+		$array['a'] = empty($arr[3])?$system['route']['default_action']:$arr[3];
+		unset($arr[3]);
+		
+		foreach ($arr as $k=>$v){
+			if($k%2 == 0){
+				$array[$v] = $arr[$k+1];
 			}
+		}
+		
+// 		debug($array,0);
+		
+// 		$this->url_query ['query'] = preg_replace ( '/\//', '', $this->url_query ['query'] );
+// 		$arr = ! empty ( $this->url_query ['query'] ) ? explode ( '-', preg_replace ( '/.html/', '', $this->url_query ['query'] ) ) : array ();
+		
+		if (count ( $array ) > 0) {
+// 			foreach ( $arr as $item ) {
+// 				$tmp = explode ( '_', $item );
+// 				$array [$tmp [0]] = $tmp [1];
+// 			}
 			
 			if (isset ( $array ['m'] )) {
 				$this->route_url ['mod'] = $array ['m'];
@@ -114,6 +135,10 @@ final class route {
 		} else {
 			$this->route_url = array ();
 		}
+		
+		//释放数组
+		unset($arr);
+		unset($array);
 	}
 }
 
